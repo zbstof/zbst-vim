ef6ccd26c22f3ecdbe1c0177b95f2406410ab555
Stanislav Bitsko <Stanislav_Bitsko@epam.com>
2012-08-20 14:30:06 +0300

    Treat JSPs as XMLs

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 ln -v bundle/xml.vim/ftplugin/xml.vim bundle/xml.vim/ftplugin/html.vim
 ln -v bundle/xml.vim/ftplugin/xml.vim bundle/xml.vim/ftplugin/xhtml.vim
+ln -v bundle/xml.vim/ftplugin/xml.vim bundle/xml.vim/ftplugin/jsp.vim
